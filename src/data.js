0e71f91da000728f020d87e0d89758e7e37b4882
KAREN RAMIREZ <karen.bussines@hotmail.com>
2019-11-19 17:07:00 -0600

    se vinculan los archivos para trabajar, se agregan las funciones de filtrar y la funcion para filtrar los datos de la ogina principal img, actor, name

--- src/data.js
+++ src/data.js
@@ -1,11 +1,24 @@
-// botton1  para ir a la siguiente Pagina
-let boton1= document.getElementById("conocemas");
-boton1.addEventListener("click",nextPage)
+export const POTTERHOLA =() =>{
+  return 'POTTER';
+};
 
+export const especies=(POTTER)=>{
+  return POTTER.filter((hola)=> {
 
-function nextPage(){
-  let pagina1= document.getElementById("pagina1").style.display = "none";
-  let pagina2= document.getElementById("pagina2").style.display = "block";
-console.log(pagina1)
+    return hola.species ==="human";
+    });
+
+} 
+
+export const allPeople =(POTTER)=>{
+ let personajes =[];
+ 
+  POTTER.forEach((element) => {
+    personajes.push({
+      imagen: POTTER.image,
+      nombre: POTTER.name,
+      actor: POTTER.actor     
+    });
+ }); 
+ return personajes;
 }
-
--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+ import POTTER from './data/potter/potter.js';
+ console.log(POTTER);
+ import {especies, allPeople} from './data.js';
+ 
+// botton1  para ir a la siguiente Pagina
+let boton1= document.getElementById ("conocemas");
 
-import { example } from './data.js';
-
-console.log(example);
-
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+boton1.addEventListener("click", () => {
+    document.getElementById("pagina1").style.display = "none";
+  document.getElementById("pagina2").style.display = "block";
+});
+  var ana =allPeople(POTTER);
+  console.log (ana)
