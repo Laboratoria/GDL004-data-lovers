4e3a80eb2d0c4aa4e35c4462982d60c06867942a
KAREN RAMIREZ <karen.bussines@hotmail.com>
2019-11-21 20:44:06 -0600

    se agrega funcion para mostrar datos en pantalla

--- src/main.js
+++ src/main.js
@@ -9,18 +9,30 @@ boton1.addEventListener("click", () => {
     document.getElementById("pagina1").style.display = "none";
   document.getElementById("pagina2").style.display = "block";
 });
-let mostrar = document.getElementById("mostrarPersonajes");
-let infoGeneral = allPeople(POTTER);
-console.log(infoGeneral)
 
-fun
+  console.log("entre")
+  let mostrar = document.getElementById("mostrarPersonajes");
+  let infoGeneral = allPeople(POTTER);
+  mostrar.onload = mostrarLista;
 
 
-for(let i=0; i<infoGeneral.length; i++){
-  let newDiv = document.createElement('div');
-  newDiv.textContent = infoGeneral[i].nombre;
-  mostrar.appendChild(newDiv);  }
+const mostrarLista= ()=>{
+  const lista= document.createElement("div")
+  lista.classList.add("contenedor");
 
+    for(let i=0; i<infoGeneral.length; i++){
+      let card = document.createElement('div');
+      card.classList.add("card"); 
+      card.innerText= infoGeneral[i].nombre+" "+ infoGeneral[i].actor;
+
+      let img=document.createElement("img");
+      card.appendChild(img)
+      img.setAttribute("src",infoGeneral[i].image);
+
+      mostrar.appendChild(lista);
+      lista.appendChild(card);
+    }
+};
 
   
 
