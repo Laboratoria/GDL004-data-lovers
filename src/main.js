9299598cf2ddaa932ac03234509b137a4cc2c770
KAREN RAMIREZ <karen.bussines@hotmail.com>
2019-11-21 22:18:00 -0600

    mostrando personajes en pantalla y estilos css

--- src/main.js
+++ src/main.js
@@ -10,32 +10,53 @@ boton1.addEventListener("click", () => {
   document.getElementById("pagina2").style.display = "block";
 });
 
-  console.log("entre")
-  let mostrar = document.getElementById("mostrarPersonajes");
-  let infoGeneral = allPeople(POTTER);
-  mostrar.onload = mostrarLista;
-
-
-const mostrarLista= ()=>{
-  const lista= document.createElement("div")
-  lista.classList.add("contenedor");
-
-    for(let i=0; i<infoGeneral.length; i++){
-      let card = document.createElement('div');
-      card.classList.add("card"); 
-      card.innerText= infoGeneral[i].nombre+" "+ infoGeneral[i].actor;
-
-      let img=document.createElement("img");
-      card.appendChild(img)
-      img.setAttribute("src",infoGeneral[i].image);
-
-      mostrar.appendChild(lista);
-      lista.appendChild(card);
-    }
-};
-
   
+let mostrar = document.getElementById("mostrarPersonajes");
+let infoGeneral = allPeople(POTTER);
+console.log(infoGeneral)
 
+const lista= document.createElement("div");
+lista.classList.add("personajes");
+
+
+
+for(let i=0; i<infoGeneral.length; i++){
+//se crean todos los elementos del html
+  let card = document.createElement('div');
+  let br = document.createElement('br');
+  let p1 = document.createElement('p');
+  let p2 = document.createElement('p');
+  let img=document.createElement("img");
+  let divImg = document.createElement('div');
+// esta linea crea el nombre del div padre
+  card.classList.add("card");
+  // esta linea le agrega la clase al div de las imagenes
+  divImg.classList.add("img-personajes"); 
+// esta linea le agrega el elemento de div de la imagen 
+  card.appendChild(divImg);
+  // esta linea le agrega el elemento de la imagen al div de la imagen.
+  divImg.appendChild(img)
+// esta linea le agrega el atributo del src a la imagen
+  img.setAttribute("src",infoGeneral[i].imagen);
+  img.setAttribute("width",150);
+  // agregando una clase al p1
+  p1.classList.add("nombre-actor-personaje");
+// esta linea le agrega al elemento p1 el nombre del personaje
+  p1.innerText = infoGeneral[i].nombre;
+// esta linea le agrega al div principal el nombre del personaje
+  card.appendChild(p1);
+// esta linea le agrega un salto de linea entre personaje y actor
+  card.appendChild(br);
+  // agregando una clase al p2
+  p2.classList.add("nombre-actor-personaje");
+// esta linea le agrega al p2  el nombre del actor
+  p2.innerText = infoGeneral[i].actor;
+// esta linea le agrega al div principal el nombre del actor
+  card.appendChild(p2);
+//esta linea le agrega todas las cards al div principal
+  lista.appendChild(card);
+}
+mostrar.appendChild(lista);
 
 
 
